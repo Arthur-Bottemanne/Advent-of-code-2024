c82ed8ed087acc9efc679fba00799fce0f72de88
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2024-12-13 08:47:43 +0100

    memoization! git add .git add .git add .!

--- 11-12/Part2/src/main.rs
+++ 11-12/Part2/src/main.rs
@@ -1,7 +1,7 @@
-use std::{fs, vec};
+use std::fs;
 use std::collections::HashMap;
 
-fn split_int_in_half(num: u64) -> Vec<u64> {
+fn split_int_in_half(num: u64) -> (u64, u64) {
     let num_str = num.to_string();
     let len = num_str.len();
     let mid = len / 2;
@@ -9,61 +9,64 @@ fn split_int_in_half(num: u64) -> Vec<u64> {
     let first_half = &num_str[..mid];
     let second_half = &num_str[mid..];
 
-    return vec![first_half.parse::<u64>().unwrap(), second_half.parse::<u64>().unwrap()];
+    return (first_half.parse::<u64>().unwrap(), second_half.parse::<u64>().unwrap());
 }
 
-fn blink(mut stone: u64, memo: &mut HashMap<(u64, u32), Vec<u64>>, iteration: u32) -> Vec<u64> {
+fn blink(stone: u64, memo: &mut HashMap<(u64, u32), u64>, iteration: u32) -> u64 {
     if iteration == 0 {
-        return vec![stone];
+        //println!("Iteration {}: stone {}, count 1", iteration, stone);
+
+        return 1;
     }
 
     if let Some(result) = memo.get(&(stone, iteration)) {
-        println!("Memoized result for stone {}: {:?}", stone, result);
+        //println!("Memoized result for stone {}: iteration {}", stone, iteration);
 
-        return result.clone();
+        return *result;
     }
 
-    let next_stones = if stone == 0 {
-        vec![1]
+    let count = if stone == 0 {
+        blink(1, memo, iteration - 1)
     }
     else if stone.to_string().len() % 2 == 0 {
-        split_int_in_half(stone)
+        let stones = split_int_in_half(stone);
+        let mut stones_count = blink(stones.0, memo, iteration - 1);
+        stones_count += blink(stones.1, memo, iteration - 1);
+        stones_count
     }
     else {
-        vec![stone * 2024]
+        blink(stone * 2024, memo, iteration - 1)
     };
 
-    let recursive_result: Vec<u64> = next_stones
-        .iter()
-        .flat_map(|&s| blink(s, memo, iteration - 1))
-        .collect();
+    //println!("Iteration {}: stone {}, count {}", iteration, stone, count);
     
-    //println!("Iteration {}: stone {}, recursive_result {:?}", iteration, stone, recursive_result);
+    memo.insert((stone, iteration), count);
 
-    memo.insert((stone, iteration), recursive_result.clone());
-
-    recursive_result
+    count
 }
 
 fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
     let file_path = "src/input";
     let input = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    let mut stones: Vec<u64> = input
+    let stones: Vec<u64> = input
         .split(" ")
         .filter_map(|s| s.parse::<u64>().ok())
         .collect();
 
-    let mut memo: HashMap<(u64, u32), Vec<u64>> = HashMap::new();
+    let mut memo: HashMap<(u64, u32), u64> = HashMap::new();
 
     let mut result = 0;
     
     for stone in stones {
-        result += blink(stone, &mut memo, 45).len();
-        println!("finished with a stone");
+        result += blink(stone, &mut memo, 75);
     }
     
     println!("result");
     println!("count : {}", result);
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
